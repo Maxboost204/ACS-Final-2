b98cfda25a0e39e3a5e50154727bef9f645cc814
Maxboost204 <johnd3014@yahoo.com>
2018-05-26 14:54:59 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,319 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Accurate Concrete Scanning is the best concrete scanning company in Winnipeg foundation repair dig">
+        
+        <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/animate.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic' rel='stylesheet' type='text/css'>
+        <title>Accurate Concrete Scanning</title> 
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/resources/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/resources/favicons/site.webmanifest">
+        <link rel="mask-icon" href="/resources/favicons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/resources/favicons/favicon.ico">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="/resources/favicons/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="row">
+                    <img src="resources/img/Logo.png" alt="Accurate concrete scanning" class="logo">
+                    <img src="resources/img/Logo.png" alt="Accurate concrete scanning" class="logo-black">
+                    <ul class="main-nav js--main-nav">
+                        <li><a href="#why-us">Why Us</a></li>
+                        <li><a href="#works">About Us</a></li>
+                        <li><a href="#testimonials">Testimonials</a></li>
+                        <li><a href="#form">Contact Us </a></li>
+                    </ul>
+                    <a class="mobile-nav-icon js--nav-icon"><i class="ion-navicon-round"></i></a>
+                </div>
+            </nav>
+            <div class="hero-text-box">
+                <h1>Scan once.<br>dig once.<br>Save time and money</h1>
+                
+                <a class="btn btn-full" href="#form">Call 204-999-5926</a>
+                <a class="btn btn-ghost" href="#form">accurateconcretescanning@outlook.com</a>
+            </div>
+            
+        </header>
+        
+        <section class="section-features js--section-features" id="why-us">  
+            <div class="row">
+                <h2>Why use our service?</h2>
+                <p class="long-copy">
+                    We are the <b>ONLY</b> scanning company in Manitoba that is using Radan Technology! 
+                </p>
+            </div>
+            
+            <div class="row js--wp-1">
+                <div class="col span-1-of-4 box">
+                    <i class="ion-wifi icon-big"></i>
+                    <h3>Radan Technology</h3>
+                    <p>
+                        We use the best technology when scanning for underground liabilities. <a href="https://www.geophysical.com/products/structurescan-mini-xt">Radan Technology</a> is the GOLD standard in structure scanning software. The equipment we use has patented data analysis software that allows us to provide the provide information in real time while providing you with 3-D reports in pdf and excel formats. No other technology comes close!
+                    </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-stopwatch-outline icon-big"></i>
+                    <h3>Ready to go in 24 hours</h3>
+                    <p>
+                        We can service any area within Winnipeg and Manitoba at any time to meet your needs and construction deadlines. We are ready to go in 24 hours! Avoid delays and costly overtime, call today!
+
+                    </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-android-warning icon-big"></i>
+                    <h3>Protect yourself and employees </h3>
+                    <p>
+                        Make sure your work area is compliant with Manitoba Safety and Health regulations. Safety should be the number one priority of employees. 
+                    </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-social-usd icon-big"></i>
+                    <h3>Stay on Budget and on time</h3>
+                    <p>
+                        Our advanced scanning technology and our 10 years of experience in the construction industy will ensure that you avoid construction delays and avoid legal liabilities. Our rates are among the lowest in the industry to make sure your project is under budget and ahead of schedule.
+                    </p>
+                </div>
+            </div>   
+        </section>
+        
+        <section class="section-testimonials" id="testimonials"> 
+            <div class="row">
+                <h2>Customer service is our number 1 priority!</h2>
+            </div>
+            <div class="row">
+                <div class="col span-1-of-3">
+                    <blockquote>
+                        Dustin and his father are our go-to guys when we start a new project
+                        <cite><img src="resources/img/customer-1.jpg" alt="Customer 1 photo">Alberto Duncan-Manitoba Hydro</cite>
+                    </blockquote>
+                </div>
+                <div class="col span-1-of-3">
+                    <blockquote>
+                       Our company will definitely use Accurate Concrete Scanning again!
+                        <cite><img src="resources/img/customer-2.jpg" alt="Customer 2 photo">Joana Silva-City of Winnipeg</cite>
+                    </blockquote>
+                </div>
+                <div class="col span-1-of-3">
+                    <blockquote>
+                        Dustin and Eugene are reliable and work hard to get the job done. They are a pleasure to work with
+                    <cite><img src="resources/img/customer-3.jpg" alt="Customer 3 photo">Milton Chapman-Forks Market</cite>
+                    </blockquote>
+                </div>
+            </div>
+        </section>
+        
+        <section class="section-work"> 
+            <ul class="work-showcase clearfix">
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/1.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/2.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/3.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/4.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+            </ul>
+            <ul class="work-showcase clearfix">
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/5.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/6.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/7.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="work-photo">
+                        <img src="resources/img/9.jpg" alt="Radan Technology">
+                    </figure>
+                </li>
+            </ul>
+        </section>
+        
+        
+        <section class="section-steps" id="works"> 
+            <div class="row">
+                <h2>About Us</h2>
+            </div>
+            <div class="row">
+                <div class="col span-1-of-2 steps-box">
+                    <img src="resources/img/Dustin.JPG" alt="Accurate Concrete Scanning Winnipeg" class="app-screen js--wp-2">
+                </div>
+                <div class="col span-1-of-2 steps-box">
+                    <h3>Dustin Stadnyk</h3>
+                    <ul class="personal-details">
+                        <li>Owner and Operator</li>
+                        <li>Family run business of over 5 years</li>
+                        <li>Red Seal Certified</li>
+                        <li>10 years construction experience</li>
+                        <li>Class of 2010 Red River Community College</li>
+                        <li>Experienced steamfitter in Residential,Commercial and Industrial piping systems</li>
+                        <li>Expert with cutting torches, pipe threaders, benders and welders</li>
+                        <li>Demonstrates effective communications skills while leading a team</li>
+                        <li>Able to multi-task while meeting construction deadlines</li>
+                        <li>A father of two and a fan of hockey and football</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row promise">
+            
+                <blockquote>"Me and my father, Eugene, has spent decades in the construction industry. Customer service, reliability and value are the lifeblood of our family business. We won't stop till our customers are 100% satisfied."</blockquote>
+            
+            </div>
+            
+        </section>
+        
+        <section class="section-form" id="form">
+            <div class="row">
+                <h2>Avoid Construction delays, Call Us Today</h2>
+            </div>
+            <div class="row">
+                <form method="post" action="mailer.php" class="contact-form">
+                    <div class="row">
+                        
+                        <?php
+                            if($_GET['success'] ==1) {
+                                echo "<div class=\"form-messages success\">Thank you! Your message has been sent!
+                            </div>";
+                            }
+
+                            if($_GET['success'] == -1) {
+                                echo "<div class=\"form-messages error\">Ooops! Something went wrong, please try again!
+                            </div>";
+                            }
+                        ?>
+                    
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="name">Name</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="text" name="name" id="name" placeholder="Your name" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="email" name="email" id="email" placeholder="Your email" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="find-us">How did you find us?</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <select name="find-us" id="find-us">
+                                <option value="friends" selected>Friends</option>
+                                <option value="search">Search engine</option>
+                                <option value="ad">Advertisement</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                              
+                            <label>Newsletter?</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="checkbox" name="news" id="news" checked> Yes, please
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label>Drop us a line</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <textarea name="message" placeholder="Your message"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label>&nbsp;</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="submit" value="Send it!">
+                        </div>
+                    </div>
+                    
+                </form>
+                
+            </div>
+        </section>
+        
+        <footer>
+            <div class="row">
+                <div class="col span-1-of-2">
+                    <ul class="social-links">
+                        <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                        <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                        <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                        <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <p>
+                    Copyright &copy; 2018 by Accurate Concrete Scanning. All rights reserved.<br>
+                
+                </p>
+            </div>
+        </footer>
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.jsdelivr.net/selectivizr/1.0.3b/selectivizr.min.js"></script>
+    <script src="vendors/js/jquery.waypoints.min.js"></script>
+    <script src="resources/js/script.js"></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113385293-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-113385293-1');
+    </script>
+
+    
+    </body>  
+    
+</html>
